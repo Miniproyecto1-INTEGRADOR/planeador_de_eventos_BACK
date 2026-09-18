7a63060caabbe24978228da72df2313ee58e63ab
Daniel Conda <daniel.conda@correounivalle.edu.co>
2026-09-18 14:19:16 -0500

    Agregar endpoint de health y conexion a base de datos

--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
+import os
 from fastapi import FastAPI
+from sqlalchemy import create_engine
 
 app = FastAPI()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-@app.get("/")
-def root():
-    return {"message": "API funcionando correctamente"}
+if not DATABASE_URL:
+    raise ValueError("No se encontró la variable DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 
-@app.get("/saludo")
-def saludo():
-    return {"message": "Hola desde FastAPI"}
+@app.get("/api/health/")
+def health_check():
+    try:
+        with engine.connect() as connection:
+            return {
+                "status": "ok",
+                "message": "API funcionando y Base de Datos conectada exitosamente"
+            }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": f"Error conectando a la BD: {str(e)}"
+        }
