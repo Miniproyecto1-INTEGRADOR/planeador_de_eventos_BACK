5ed03e222f309cdabba506dcb386032d35727441
Dannycalvo6464 <ramirez.danny@correounivalle.edu.co>
2026-09-21 06:57:37 -0500

    Configurar CORS para integrar frontend y backend

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 import os
 from fastapi import FastAPI
 from sqlalchemy import create_engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://planeador-de-eventos-fronted.vercel.app",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
